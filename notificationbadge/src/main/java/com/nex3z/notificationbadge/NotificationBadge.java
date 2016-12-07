d9a2ac7e4835e146e5364f646e3ebc1750733273
nex3z <litianxing9@gmail.com>
2016-12-07 21:25:45 +0800

    Initial commit

---
+++ notificationbadge/src/main/java/com/nex3z/notificationbadge/NotificationBadge.java
@@ -0,0 +1,209 @@
+package com.nex3z.notificationbadge;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class NotificationBadge extends FrameLayout {
+    private static final int DEFAULT_TEXT_COLOR = 0xFFFFFFFF;
+    private static final int DEFAULT_TEXT_SIZE = 14;
+    private static final boolean DEFAULT_ANIMATION_ENABLED = true;
+    private static final int DEFAULT_ANIMATION_DURATION = 500;
+
+    private FrameLayout mContainer;
+    private ImageView mIvBadgeBg;
+    private TextView mTvBadgeText;
+    private int mBadgeTextColor;
+    private float mBadgeTextSize;
+    private int mAnimationDuration;
+    private Animation mUpdate;
+    private Animation mShow;
+    private Animation mHide;
+    private String mBadgeText;
+    private boolean mIsBadgeShown;
+    private boolean mAnimationEnabled;
+
+    public NotificationBadge(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.notification_badge, this, true);
+        mContainer = (FrameLayout) findViewById(R.id.fl_container);
+        mIvBadgeBg = (ImageView) findViewById(R.id.iv_badge_bg);
+        mTvBadgeText = (TextView) findViewById(R.id.tv_badge_text);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs, R.styleable.NotificationBadge, 0, 0);
+        try {
+            mBadgeTextColor = a.getColor(R.styleable.NotificationBadge_badgeTextColor, DEFAULT_TEXT_COLOR);
+            mTvBadgeText.setTextColor(mBadgeTextColor);
+
+            mBadgeTextSize = a.getDimensionPixelSize(R.styleable.NotificationBadge_badgeTextSize, (int)dpToPx(DEFAULT_TEXT_SIZE));
+            mTvBadgeText.setTextSize(TypedValue.COMPLEX_UNIT_PX, mBadgeTextSize);
+
+            mAnimationEnabled = a.getBoolean(R.styleable.NotificationBadge_animationEnabled, DEFAULT_ANIMATION_ENABLED);
+
+            mAnimationDuration = a.getInt(R.styleable.NotificationBadge_animationDuration, DEFAULT_ANIMATION_DURATION);
+            Drawable badgeBackground = a.getDrawable(R.styleable.NotificationBadge_badgeBackground);
+            if (badgeBackground != null) {
+                mIvBadgeBg.setImageDrawable(badgeBackground);
+            }
+        } finally {
+            a.recycle();
+        }
+
+        if (mAnimationEnabled) {
+            initUpdateAnimation();
+        }
+    }
+
+    public void clear() {
+        if (mIsBadgeShown) {
+            if (mAnimationEnabled) {
+                mContainer.startAnimation(mHide);
+            } else {
+                mContainer.setVisibility(INVISIBLE);
+            }
+            mIsBadgeShown = false;
+        }
+    }
+
+    public void show(String text) {
+        mBadgeText = text;
+        if (!mIsBadgeShown) {
+            if (mAnimationEnabled) {
+                mContainer.startAnimation(mShow);
+            } else {
+                mContainer.setVisibility(VISIBLE);
+                mTvBadgeText.setText(text);
+            }
+            mIsBadgeShown = true;
+        } else {
+            mTvBadgeText.setText(text);
+        }
+    }
+
+    public boolean isAnimationEnabled() {
+        return mAnimationEnabled;
+    }
+
+    public void setAnimationEnabled(boolean animationEnabled) {
+        mAnimationEnabled = animationEnabled;
+        if (animationEnabled && (mUpdate == null || mShow == null || mHide == null)) {
+            initUpdateAnimation();
+        }
+    }
+
+    public void setText(String text) {
+        mBadgeText = text;
+        if (text == null || text.isEmpty()) {
+            clear();
+        } else {
+            if (mIsBadgeShown) {
+                if (mAnimationEnabled) {
+                    mContainer.startAnimation(mUpdate);
+                } else {
+                    mTvBadgeText.setText(text);
+                }
+            } else {
+                show(text);
+            }
+        }
+    }
+
+    public void setNumber(int number) {
+        if (number == 0) {
+            clear();
+        } else {
+            setText(String.valueOf(number));
+        }
+    }
+
+    public int getTextColor() {
+        return mBadgeTextColor;
+    }
+
+    public void setTextColor(int textColor) {
+        mBadgeTextColor = textColor;
+        mTvBadgeText.setTextColor(textColor);
+    }
+
+    public void setTextSize(float textSize) {
+        mBadgeTextSize = textSize;
+        mTvBadgeText.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+    }
+
+    public void setBadgeBackgroundDrawable(Drawable drawable) {
+        mIvBadgeBg.setImageDrawable(drawable);
+    }
+
+    public void setBadgeBackgroundResource(int resId) {
+        mIvBadgeBg.setImageResource(resId);
+    }
+
+    private void initUpdateAnimation() {
+        mUpdate = new ScaleAnimation(1, 1.2f, 1, 1.2f,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        mUpdate.setDuration(mAnimationDuration / 2);
+        mUpdate.setRepeatMode(Animation.REVERSE);
+        mUpdate.setRepeatCount(1);
+        mUpdate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                mTvBadgeText.setText(mBadgeText);
+            }
+        });
+
+        mShow = new ScaleAnimation(0, 1, 0, 1,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        mShow.setDuration(mAnimationDuration / 2);
+        mShow.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                mContainer.setVisibility(VISIBLE);
+                mTvBadgeText.setText(mBadgeText);
+            }
+            @Override
+            public void onAnimationEnd(Animation animation) {}
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+
+        mHide = new ScaleAnimation(1, 0, 1, 0,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        mHide.setDuration(mAnimationDuration / 2);
+        mHide.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mContainer.setVisibility(INVISIBLE);
+            }
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+    }
+
+    private float dpToPx(float dp){
+        return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
+    }
+
+}
---
+++ sample/src/main/java/com/nex3z/notificationbadge/sample/MainActivity.java
@@ -0,0 +1,38 @@
+package com.nex3z.notificationbadge.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.nex3z.notificationbadge.NotificationBadge;
+
+public class MainActivity extends AppCompatActivity {
+
+    NotificationBadge mBadge;
+    private int mCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBadge = (NotificationBadge) findViewById(R.id.badge);
+
+        Button increase = (Button) findViewById(R.id.increase);
+        increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBadge.setNumber(++mCount);
+            }
+        });
+
+        Button clear = (Button) findViewById(R.id.clear);
+        clear .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBadge.clear();
+            }
+        });
+    }
+}
